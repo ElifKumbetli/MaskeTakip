61d561f5a57cdfc00466d7b1d35ef62000baf482
ElifKumbetli <elifkumbetli@gmail.com>
2023-08-13 01:13:20 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Workaround
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Degiskenler();
+
+            Vatandas vatandas1 = new Vatandas();
+            vatandas1.
+            
+
+            Console.ReadLine();
+        }
+
+        private static void Degiskenler()
+        {
+            string mesaj = "Merhaba";
+            double tutar = 100000; //db den gelir
+            int sayi = 100;
+            bool girisYapmisMi = false;
+
+            string ad = "Engin";
+            string soyad = "Demiroğ";
+            int dogumYili = 1985;
+            long tcNo = "12345678910";
+
+
+
+        Console.WriteLine(tutar * 1.18);
+
+        Console.WriteLine(tutar * 1.18);
+        }
+    }
+
+    //pascal casing
+
+    public class Vatandas
+    {
+        public string Ad { get; set; }
+
+        public string Soyad { get; set; }
+
+        public int DogumYili { get; set; }
+
+        public long TcNo { get; set; }
+    }
+}
